25156e8d830154dbe70bb741c4cb8f7ed6fe1334
estrada3 <estrada3@wwu.edu>
2019-02-10 22:29:50 -0800

    adding quiz 4

---
+++ Lesson12/js/main.js
@@ -0,0 +1,8 @@
+//call the function
+functioname();
+
+function doSomething(){
+    //document.getElementById('java').innerHTML = "New JavaScript For Class";
+    var x = document.getElementById('java');
+    x.style.color = "red";
+}
